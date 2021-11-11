304963f601949804f9aa854097df580c11e304c7
Bo Zhang <bo@gradle.com>
2021-11-11 15:41:09 +0800

    Avoid unnecessary Gradleception builds
    Previously, Gradleception runs for every commit, even there's only
    test or doc changes. This change avoids these builds by:
    
    - In first `install` step, use fixed commit id/timestamp so that
    the generated distribution is only affected by production code change.
    - Before second `install` step, a script is executed to calculate the
    distribution's md5.
    - Use that md5 as second `install` step's version number.
    
    In this way, with non-production code changes, the two install steps
    generate identical distribution with identical version.

--- .teamcity/src/main/kotlin/configurations/Gradleception.kt
+++ .teamcity/src/main/kotlin/configurations/Gradleception.kt
@@ -4,6 +4,7 @@ import common.buildToolGradleParameters
 import common.customGradle
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildSteps
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.GradleBuildStep
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import model.CIBuildModel
 import model.Stage
 
@@ -20,15 +21,31 @@ class Gradleception(model: CIBuildModel, stage: Stage) : BaseGradleBuildType(sta
         javaCrash = false
     }
 
+    params {
+        // Override the default commit id so the build steps produce reproducible distribution
+        param("env.BUILD_COMMIT_ID", "HEAD")
+    }
+
+    // So that the build steps produces reproducible distribution
+    val magicTimestamp = "20210102030405+0000"
     val buildScanTagForType = buildScanTag("Gradleception")
     val defaultParameters = (buildToolGradleParameters() + listOf(buildScanTagForType) + "-Porg.gradle.java.installations.auto-download=false").joinToString(separator = " ")
 
-    applyDefaults(model, this, ":distributions-full:install", notQuick = true, extraParameters = "-Pgradle_installPath=dogfood-first $buildScanTagForType", extraSteps = {
+    applyDefaults(model, this, ":distributions-full:install", notQuick = true, extraParameters = "-Pgradle_installPath=dogfood-first -PignoreIncomingBuildReceipt=true -PbuildTimestamp=$magicTimestamp $buildScanTagForType", extraSteps = {
+        script {
+            name = "CALCULATE_DISTRIBUTION_MD5"
+            scriptContent = """
+                set -x
+                MD5=`find %teamcity.build.checkoutDir%/dogfood-first -type f | sort | xargs md5sum | md5sum | awk '{ print ${'$'}1 }'`
+                echo "##teamcity[setParameter name='env.ORG_GRADLE_PROJECT_versionQualifier' value='gradleception-${'$'}MD5']"
+            """.trimIndent()
+        }
+
         localGradle {
             name = "BUILD_WITH_BUILT_GRADLE"
             tasks = "clean :distributions-full:install"
             gradleHome = "%teamcity.build.checkoutDir%/dogfood-first"
-            gradleParams = "-Pgradle_installPath=dogfood-second -PignoreIncomingBuildReceipt=true $defaultParameters"
+            gradleParams = "-Pgradle_installPath=dogfood-second -PignoreIncomingBuildReceipt=true -PbuildTimestamp=$magicTimestamp $defaultParameters"
         }
         localGradle {
             name = "QUICKCHECK_WITH_GRADLE_BUILT_BY_GRADLE"
